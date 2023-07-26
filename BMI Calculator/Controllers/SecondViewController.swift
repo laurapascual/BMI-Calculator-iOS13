708c2ea991c633e19939303a6806c8f5e1f4275b
Laura Pascual <laurapascualmateo@gmail.com>
2023-07-26 18:34:01 +0200

    Add secondView

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Pascual Mateo Laura on 26/7/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+}
